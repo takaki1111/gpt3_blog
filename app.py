fada39a29cf7ba78b7a7f168711c54f0506b9ed4
takaki1111 <takahashi@bloom-t.co.jp>
2022-10-16 14:02:36 +0900

    2つの機能を追加

--- app.py
+++ app.py
@@ -4,39 +4,67 @@ import requests
 import datetime
 import openai
 
-API_KEY=st.secrets.OpenAI.API_KEY
-openai.api_key = API_KEY
 
-def text_summary(prompt,sum_str):
-    # 分析の実施
+#API_KEY=st.secrets.OpenAI.API_KEY
+openai.api_key = "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"
+
+def make_sentence(prompt,sum_str,temperature):
     response = openai.Completion.create(
     engine="text-davinci-002",
     prompt=prompt,
-    temperature=0.8,
+    temperature=temperature,
     max_tokens=sum_str,
     top_p=1.0,
-    frequency_penalty=0.0,
+    frequency_penalty=0.5,
     presence_penalty=0.0,
-    #stop=["あなた:", "高橋:"]
     )
 
     # 分析結果の出力
     return response["choices"][0]["text"].replace('\n','')
 
-prompt="Blog\nTitle:1\
-\ntags:2\
-\nSection:3\
-\nFull text:"
-
-
-
 st.header("ブログ記事ライティング")
-title = st.text_input("ブログ記事のタイトル","") 
-tags = st.text_input("見出し記事のタグ(含めたいキーワード)","") 
-section = st.text_input("記事の見出し名","") 
-sum_str =st.slider("生成する文字数", 0, 3000, 1000, 1)
-if title and tags and section:
-    prompt_input = prompt.translate(str.maketrans({'1': title, '2': tags,'3': section}))
-    return_sent = text_summary(prompt_input,sum_str)
 
-    st.text_area("見出し記事本文",return_sent, height=700,max_chars=3500)
+mh1 = '記事のタイトル + タグ + 記事の見出し名'
+mh2 = '記事のタイトル + イントロ + 記事の見出し名 + 見出しの冒頭文章'
+
+genre = st.radio(
+     "以下の2つから見出し記事本文の生成方法を選んでください。",
+     (mh1, mh2))
+
+if genre == mh1:
+
+    title = st.text_input("ブログ記事のタイトル","") 
+    tags = st.text_input("見出し記事のタグ(含めたいキーワード)","") 
+    section = st.text_input("記事の見出し名","")
+    sum_str =st.slider("生成する最大文字数", 0, 3000, 1000, 1)
+    temperature = st.slider("出現させる単語のランダム性", 0.0, 2.0, 0.80, 0.05)
+
+    prompt = "Blog\nTitle:X\ntags:Y\nSection:Z\nFull text:"
+    prompt_input = prompt.translate(str.maketrans({'X': title, 'Y': tags,'Z': section}))
+    st.write(prompt_input)
+
+    
+
+else:
+
+    title = st.text_input("ブログ記事のタイトル","") 
+    intr = st.text_input("記事のイントロ(導入文)を2～3行程度","") 
+    section = st.text_input("記事の見出し名","")
+    sec_sentc = st.text_input("見出しの冒頭文章(2～3行くらいの文量)","")
+
+    sum_str =st.slider("生成する最大文字数", 0, 3000, 1000, 1)    
+    temperature = st.slider("出現させる単語のランダム性", 0.0, 2.0, 0.80, 0.05)
+
+    prompt = "Blog\nTitle:X\nIntro:Y\nSection:Z\nFull text:R"
+    prompt_input = prompt.translate(str.maketrans({'X': title, 'Y': intr,'Z': section,'R': sec_sentc}))
+    #st.write(prompt_input)
+
+
+
+
+if st.button('見出し記事本文生成'):
+    full_text = make_sentence(prompt_input,sum_str,temperature)
+    st.text_area(label='見出し記事本文', value=full_text, height=700,max_chars=3500)
+else:
+    st.text_area(label='見出し記事本文', value="", height=700,max_chars=3500)
+
