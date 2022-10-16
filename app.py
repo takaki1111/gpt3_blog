aaf589465c5f4cbaf0b76e9e502ffcbeff9f5acd
takaki1111 <takahashi@bloom-t.co.jp>
2022-10-16 15:04:18 +0900

    一部のtext_inputをtext_areaに変更

--- app.py
+++ app.py
@@ -47,9 +47,11 @@ if genre == mh1:
 else:
 
     title = st.text_input("ブログ記事のタイトル","") 
-    intr = st.text_input("記事のイントロ(導入文)を2～3行程度","") 
+    #intr = st.text_input("記事のイントロ(導入文)を2～3行程度","") 
+    intr = st.text_area(label="記事のイントロ(導入文)を2～3行程度",height=50) 
     section = st.text_input("記事の見出し名","")
-    sec_sentc = st.text_input("見出しの冒頭文章(2～3行くらいの文量)","")
+    #sec_sentc = st.text_input("見出しの冒頭文章(2～3行くらいの文量)","")
+    sec_sentc = st.text_area(label="見出しの冒頭文章(2～3行くらいの文量)",height=50)
     sum_str =st.slider("生成する最大文字数", 0, 3000, 1000, 1)    
     temperature = st.slider("出現させる単語のランダム性", 0.0, 2.0, 0.80, 0.05)
     ini_text = sec_sentc
