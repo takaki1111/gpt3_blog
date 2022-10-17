25cc62c178bad18ea71e78d822bb72e1900ed585
takaki1111 <takahashi@bloom-t.co.jp>
2022-10-17 23:21:31 +0900

    文章要約を追加

--- app.py
+++ app.py
@@ -24,12 +24,14 @@ def make_sentence(prompt,sum_str,temperature):
 
 st.header("ブログ記事ライティング")
 
-mh1 = '記事のタイトル + タグ + 記事の見出し名'
-mh2 = '記事のタイトル + イントロ + 記事の見出し名 + 見出しの冒頭文章'
+mh1 = '①記事本文生成[記事のタイトル + タグ + 記事の見出し名]'
+mh2 = '②記事本文生成[記事のタイトル + イントロ + 記事の見出し名 + 見出しの冒頭文章]'
+mh3 = '③文章の要約'
+
 
 genre = st.radio(
-     "以下の2つから見出し記事本文の生成方法を選んでください。",
-     (mh1, mh2))
+     "以下の3つから行いたい文章生成のタスクを選んでください。",
+     (mh1,mh2,mh3))
 
 if genre == mh1:
 
@@ -41,10 +43,15 @@ if genre == mh1:
     ini_text = ""
     prompt = "Blog\nTitle:X\ntags:Y\nSection:Z\nFull text:"
     prompt_input = prompt.translate(str.maketrans({'X': title, 'Y': tags,'Z': section}))
+    button_name = '記事本文を生成させる'
+    if st.button(button_name):
+            full_text = make_sentence(prompt_input,sum_str,temperature)
+            #full_text = ini_text + full_text
+            st.text_area(label='記事本文', value=full_text, height=700,max_chars=3500)
 
     
 
-else:
+elif genre == mh2:
 
     title = st.text_input("ブログ記事のタイトル","") 
     #intr = st.text_input("記事のイントロ(導入文)を2～3行程度","") 
@@ -57,14 +64,29 @@ else:
     ini_text = sec_sentc
     prompt = "Blog\nTitle:X\nIntro:Y\nSection:Z\nFull text:R"
     prompt_input = prompt.translate(str.maketrans({'X': title, 'Y': intr,'Z': section,'R': sec_sentc}))
+    button_name = '記事本文を生成させる'
+    if st.button(button_name):
+            full_text = make_sentence(prompt_input,sum_str,temperature)
+            #full_text = ini_text + full_text
+            st.text_area(label='記事本文', value=full_text, height=700,max_chars=3500)
 
+    
 
-
-
-if st.button('見出し記事本文生成'):
-    full_text = make_sentence(prompt_input,sum_str,temperature)
-    full_text = ini_text + full_text
-    st.text_area(label='見出し記事本文', value=full_text, height=700,max_chars=3500)
 else:
-    st.text_area(label='見出し記事本文', value="", height=700,max_chars=3500)
+    input_text = st.text_area(label="要約する元のテキスト",height=300) 
+    sum_str =st.slider("生成する最大文字数", 0, 3000, 1000, 1)    
+    temperature = st.slider("出現させる単語のランダム性", 0.0, 2.0, 0.80, 0.05)
+    point = st.slider("要約で得たい文章の数", 1, 7, 3, 1)
+
+    prompt ='''tl;dr:ポイントは以下のXXX点です。'''
+    prompt = prompt.replace('XXX',str(point))
+    input_text = input_text.replace('\n','')
+    prompt_input = input_text + prompt
+    button_name = '文章を要約する'
+
+        
+    if st.button(button_name):
+        full_text = make_sentence(prompt_input,sum_str,temperature)
+        #full_text = ini_text + full_text
+        st.text_area(label='要約文', value=full_text, height=700,max_chars=3500)
 
